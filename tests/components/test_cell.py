60ae88a6dc781c7c8f55c1159e7ab460abd6e4fa
Akhila Ananth <akhilasananth@gmail.com>
2026-02-08 16:43:26 +0400

    format update

--- tests/components/test_cell.py
+++ tests/components/test_cell.py
@@ -7,30 +7,30 @@ def test_cell():
     return Cell(0,0)
 
 def test_tostr_all_walls(test_cell):
-    assert str(test_cell) == "---\n│ │\n---"
+    assert str(test_cell) == "+---+\n│   │\n+---+"
 
 def test_tostr_no_north_wall(test_cell):
     test_cell.walls[Direction.NORTH] = False
-    assert str(test_cell) == "│ │\n---"
+    assert str(test_cell) == "+   +\n│   │\n+---+"
 
 def test_tostr_no_south_wall(test_cell):
     test_cell.walls[Direction.SOUTH] = False
-    assert str(test_cell) == "---\n│ │"
+    assert str(test_cell) == "+---+\n│   │\n+   +"
 
 def test_tostr_no_west_wall(test_cell):
     test_cell.walls[Direction.WEST] = False
-    assert str(test_cell) == "---\n  │\n---"
+    assert str(test_cell) == "+---+\n    │\n+---+"
 
 def test_tostr_no_east_wall(test_cell):
     test_cell.walls[Direction.EAST] = False
-    assert str(test_cell) == "---\n│  \n---"
+    assert str(test_cell) == "+---+\n│    \n+---+"
 
 def test_tostr_no_walls(test_cell):
     for direction in Direction:
         test_cell.walls[direction] = False
-    assert str(test_cell) == "   "
+    assert str(test_cell) == "+   +\n     \n+   +"
 
 def test_tostr_remove_wall(test_cell):
     test_cell.remove_wall(Direction.NORTH)
     assert test_cell.walls[Direction.NORTH] == False
-    assert str(test_cell) == "│ │\n---"
+    assert str(test_cell) == "+   +\n│   │\n+---+"
